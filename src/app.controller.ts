709fc425961a554732af300ddf84e379ec538e08
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-09-28 16:46:47 -0300

    feat: add create and list notifications routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor() {}
+  constructor(
+    private readonly prismaService: PrismaService
+  ) {}
+
+  @Get()
+  async list() {
+    return await this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(
+    @Body() body: CreateNotificationBody
+  ) {
+    const { content, category, recipientId } = body
+
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      }
+    });
+  }
 }
